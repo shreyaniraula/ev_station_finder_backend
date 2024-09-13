24bd92ab57d74b9afc0301dab18f686451f93c6e
Shreya <shreeya.775421@mmamc.tu.edu.np>
2024-09-13 09:57:51 +0545

    user logout added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js'
 import { upload } from '../middlewares/multer.middleware.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 const userRouter = Router()
 
@@ -15,6 +16,6 @@ userRouter.route('/register').post(
 )
 
 userRouter.route('/login').post(loginUser)
-userRouter.route('/logout').post(logoutUser)
+userRouter.route('/logout').post(verifyJWT, logoutUser)
 
 export default userRouter
